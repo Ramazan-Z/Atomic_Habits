8d91b27da063cc6b3ec9d08b7b49335f203f72c4
Ramazan-Z <Ramazan_zalilov@mail.ru>
2024-12-12 18:09:50 +0400

    add other endpoints

--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,8 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
     ],
 }
 
--- habits/serializers.py
+++ habits/serializers.py
@@ -23,6 +23,18 @@ class HabitSerializer(ModelSerializer):
         moment = MomentHabit.objects.create(**moment_data)
         return Habit.objects.create(moment=moment, **validated_data)
 
+    def update(self, instance, validated_data):
+        """Обновление объекта вложенного поля (времени привычки)"""
+        moment_data = validated_data.pop("moment")
+        moment = instance.moment
+        for field, value in moment_data.items():
+            setattr(moment, field, value)
+        moment.save()
+        for field, value in validated_data.items():
+            setattr(instance, field, value)
+        instance.save()
+        return instance
+
     def get_validators(self):
         """Список валидаторов для полей is_pleasant, award и related_habit"""
         return [is_pleasant_validator, only_one_award_validator, RelatedHabitValidator(self.context)]
